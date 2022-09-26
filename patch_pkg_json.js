45ed3fd97d379d327bffae490228da65d6c0e57e
Florian Loers <florianloers@mailbox.org>
2022-09-26 11:28:18 +0200

    fix: Fix wrong typescript types and add CI checks for that

--- patch_pkg_json.js
+++ patch_pkg_json.js
@@ -1,7 +1,13 @@
-const fs = require("fs"); // replace with import fs from 'fs'; if you need
-const packageFileContent = fs.readFileSync("./pkg/package.json", "utf-8");
-const packageJSON = JSON.parse(packageFileContent);
-packageJSON.type = "module";
-packageJSON.main = packageJSON.module;
-packageJSON.exports = "./" + packageJSON.module;
-fs.writeFileSync("./pkg/package.json", JSON.stringify(packageJSON, null, 2), "utf-8");
+const fs = require('fs') // replace with import fs from 'fs'; if you need
+const packageFileContent = fs.readFileSync('./pkg/package.json', 'utf-8')
+const packageJSON = JSON.parse(packageFileContent)
+packageJSON.type = 'module'
+packageJSON.main = packageJSON.module
+packageJSON.exports = './' + packageJSON.module
+packageJSON.dependencies = { typescript: '^4.8.3' }
+packageJSON.scripts = { tsc: 'tsc owlish.d.ts' }
+fs.writeFileSync(
+    './pkg/package.json',
+    JSON.stringify(packageJSON, null, 2),
+    'utf-8'
+)
